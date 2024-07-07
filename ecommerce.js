a523973c12decc5524d0b70f6c69366b5e1d8c25
Jetter <jlgarcia2@student.apc.edu.ph>
2024-07-07 14:32:42 +0800

    Refactor guitar inventory data structure

--- ecommerce.js
+++ ecommerce.js
@@ -1,44 +1,47 @@
-let G10001 = {
-  model: "Les Paul Standard '50s",
-  type: "Electric Guitar",
-  pickups: "Burstbucker 1 & 2",
-  price: 2499.0,
-  color: "Tobacco Burst",
-  stock: 10,
-};
-
-let G10002 = {
-  model: "SG Standard",
-  type: "Electric Guitar",
-  pickups: "490R & 490T",
-  price: 1499.0,
-  color: "Ebony",
-  stock: 10,
-};
-
-let G10003 = {
-  model: "ES-335 Figured",
-  type: "Electric Guitar",
-  pickups: "MHS II Humbuckers",
-  price: 3699.0,
-  color: "Sixties Cherry",
-  stock: 10,
-};
-
-let G10004 = {
-  model: "J-45 Standard",
-  type: "Acoustic Guitar",
-  body: "Round Shoulder Dreadnought",
-  price: 2499.0,
-  color: "Vintage Sunburst",
-  stock: 10,
-};
-
-let G10005 = {
-  model: "Flying V",
-  type: "Electric Guitar",
-  pickups: "Dirty Fingers+ Humbuckers",
-  price: 1899.0,
-  color: "Ebony",
-  stock: 10,
-};
+let products = [
+  {
+    id: 1,
+    model: "Les Paul Standard '50s",
+    type: "Electric Guitar",
+    pickups: "Burstbucker 1 & 2",
+    price: 2499.0,
+    color: "Tobacco Burst",
+    stock: 10,
+  },
+  {
+    id: 2,
+    model: "SG Standard",
+    type: "Electric Guitar",
+    pickups: "490R & 490T",
+    price: 1499.0,
+    color: "Ebony",
+    stock: 10,
+  },
+  {
+    id: 3,
+    model: "ES-335 Figured",
+    type: "Electric Guitar",
+    pickups: "MHS II Humbuckers",
+    price: 3699.0,
+    color: "Sixties Cherry",
+    stock: 10,
+  },
+  {
+    id: 4,
+    model: "J-45 Standard",
+    type: "Acoustic Guitar",
+    body: "Round Shoulder Dreadnought",
+    price: 2499.0,
+    color: "Vintage Sunburst",
+    stock: 10,
+  },
+  {
+    id: 5,
+    model: "Flying V",
+    type: "Electric Guitar",
+    pickups: "Dirty Fingers+ Humbuckers",
+    price: 1899.0,
+    color: "Ebony",
+    stock: 10,
+  },
+];
